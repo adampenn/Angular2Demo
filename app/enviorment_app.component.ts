e4d18593081751cf473f1a57a323ebc8b9c5b828
Adam J. Penn <madannep@gmail.com>
2017-03-31 00:20:14 +0000

    adding inital files to transfer to machine thats not a server

---
+++ app/enviorment_app.component.ts
@@ -0,0 +1,14 @@
+import {Componenet, View} from "angular2/core";
+
+@Componenet({
+  selectorL 'my-app'
+})
+
+@View({
+  template: '<h2>Angluar 2 app</h2>'
+})
+
+export class AppComponent {
+
+}
+
---
+++ app/enviorment_main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from "angular2/platform/browser"
+import {AppComponent) from "./enviorment_app.component"
+
+bootstrap(AppComponent);
